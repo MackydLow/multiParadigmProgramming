6c3ed1d5f543ed7873d71129650f9583abf992a6
MackydLow <2636498@dundee.ac.uk>
2025-12-05 14:52:39 +0000

    save

--- c++ final project/member.cpp
+++ c++ final project/member.cpp
@@ -2,15 +2,42 @@
 #include <string>
 #include "book.h"
 #include "member.h"
+#include <vector>
 using namespace std;
-
-class Member {
-    private:
-        int memberID;
-        string name;
-        string Book[];
-}
  
-void borrowBook() {
-    
+//getters
+int Member::getMemberID(){
+     return memberID;
 }
+
+string Member::getName(){
+     return name;
+}
+
+//setters
+void Member::setMemberID(int newMemberID) {
+    memberID = newMemberID;
+}
+
+void Member::setName(string newName) {
+    name = newName;
+}
+
+//functions
+void Member::borrowBook(Book &book) {
+    currentBorrow[currentBorrow.size()] = book;
+
+    book.setAvaliability(false);
+}
+
+void Member::returnBook(Book &book) {
+    for (size_t i = 0; i < currentBorrow.size(); i++) {
+        if (currentBorrow[i].getBookID() == book.getBookID()) {
+            cout << i << endl;
+            currentBorrow.erase(currentBorrow.begin() + i);
+        }
+    }
+
+}
+
+
---
+++ c++ final project/member.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "book.h"
+#include "member.h"
+#include <vector>
+using namespace std;
+
+class Member {
+    private:
+        int memberID;
+        string name;
+        std::vector<Book> currentBorrow;
+    public:
+        int getMemberID();
+        string getName();
+        void setMemberID(int newMemberID);
+        void setName(string newName);
+        void borrowBook(Book &book);
+        void returnBook(Book &book);
+
+};
